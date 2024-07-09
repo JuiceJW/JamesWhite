0f293bf4b8e47148f96ddba13abb6bdef7225d84
James <42639609+JuiceJW@users.noreply.github.com>
2024-07-09 18:02:29 +1200

    Add contact section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,8 +5,8 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "James White" },
+    { name: "description", content: "Full Stack Software Engineer" },
   ];
 };
 
@@ -36,6 +36,47 @@ export default function Index() {
             I am James White, a full stack software engineer based in Auckland,
             New Zealand.
           </p>
+
+          <p className="text-xl">
+            Bringing four years of comprehensive experience in full-stack
+            development to the table, I have honed my skills across a diverse
+            range of programming languages and frameworks. My expertise spans
+            JavaScript, TypeScript, and Kotlin, enabling me to craft and deploy
+            scalable software solutions efficiently. My journey in the tech
+            industry has been marked by a relentless pursuit of knowledge and a
+            deep commitment to developing applications that not only meet but
+            exceed user expectations.
+          </p>
+        </div>
+      </section>
+
+      {/* <section id="portfolio" className="container mx-auto my-16">
+        <div className="flex flex-col gap-16 p-4">
+          <h1 className="text-6xl font-normal">Portfolio.</h1>
+
+          <p className="text-xl">
+            Here are a few of the projects I have worked on.
+          </p>
+        </div>
+      </section>
+
+      <section id="technologies" className="container mx-auto my-16">
+        <div className="flex flex-col gap-16 p-4">
+          <h1 className="text-6xl font-normal">Technologies.</h1>
+
+          <p className="text-xl">
+            Here are a few of the technologies I have worked with.
+          </p>
+        </div>
+      </section> */}
+
+      <section id="contact" className="container mx-auto my-16">
+        <div className="flex flex-col gap-16 p-4">
+          <h1 className="text-6xl font-normal">Contact.</h1>
+
+          <p className="text-xl">
+            Get in touch with me through linkedin on the bottom right.
+          </p>
         </div>
       </section>
 
@@ -43,10 +84,21 @@ export default function Index() {
         <div className="container mx-auto">
           <div className="flex justify-end items-center p-4">
             <div className="flex gap-4">
-              <a href="https://www.linkedin.com/in/james-dylan-white-8a2b6a1b2/">
+              <span className="justify-self-center self-center">
+                © James White 2024
+              </span>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/jamesdylanwhite/"
+              >
                 <FaLinkedin size={48} />
               </a>
-              <a href="https://www.linkedin.com/in/james-dylan-white-8a2b6a1b2/">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/JuiceJW"
+              >
                 <FaGithub size={48} />
               </a>
             </div>
