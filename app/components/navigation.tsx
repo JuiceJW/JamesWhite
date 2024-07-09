aa54a3b59ecddf67da8d683b6682aed7dfa865d2
James <42639609+JuiceJW@users.noreply.github.com>
2024-07-09 17:51:13 +1200

    remove old

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,23 +1,77 @@
-export default function Navigation() {
+import { useState } from "react";
+
+const HamburgerButton = ({
+  isOpen,
+  toggle,
+}: {
+  isOpen: boolean;
+  toggle: () => void;
+}) => {
+  const commonClasses =
+    "bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm";
+
   return (
-    <nav className="fixed bg-white w-full h-20 z-50">
+    <button
+      onClick={toggle}
+      className="flex flex-col justify-center items-center w-10 h-10 lg:hidden"
+    >
+      <span
+        className={`${commonClasses} ${
+          isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+        }`}
+      ></span>
+      <span
+        className={`${commonClasses} my-0.5 ${
+          isOpen ? "opacity-0" : "opacity-100"
+        }`}
+      ></span>
+      <span
+        className={`${commonClasses} ${
+          isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+        }`}
+      ></span>
+    </button>
+  );
+};
+
+export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="fixed bg-white w-full h-20 z-50 px-16">
       <div className="flex justify-between items-center h-20 container m-auto">
         <div>
           <span className="font-semibold">James</span> White
         </div>
-        {/* <div className="flex">
-          <div
-            className="relative flex justify-center items-center w-[30px] h-[30px] cursor-pointer transition-all ease-in-out mr-4"
-            // onClick={() => {}}
-          >
-            <div className="menu-btn-burger">HB</div>
-          </div>
-        </div> */}
-        <div className="block relative">
+
+        <HamburgerButton isOpen={isOpen} toggle={handleClick} />
+
+        <div className="hidden lg:block relative">
           <ul className="flex gap-6 list-none p-0">
-            <li>About</li>
-            <li>Portfolio</li>
-            <li>Development Technologies</li>
+            <li>
+              <a href="#about-me">About</a>
+            </li>
+            {/* <li>Portfolio</li>
+            <li>Development Technologies</li> */}
+          </ul>
+        </div>
+
+        <div
+          className={`absolute top-full left-0 w-full container px-16 bg-white shadow-md lg:hidden ${
+            isOpen ? "flex" : "hidden"
+          }`}
+        >
+          <ul className="flex flex-col list-none p-4">
+            <li>
+              <a href="#about-me" onClick={() => setIsOpen(false)}>
+                About
+              </a>
+            </li>
+            {/* Additional menu items here */}
           </ul>
         </div>
       </div>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import Navigation from "~/components/navigation";
 import AucklandSkyline from "../../public/images/auckland-skyline.webp";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,7 +13,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="font-sans m-0 p-0">
-      {/* <Navigation /> */}
+      <Navigation />
       <section className="h-[100vh]">
         <img
           className="h-[100vh] w-full object-cover grayscale brightness-50"
@@ -20,12 +21,39 @@ export default function Index() {
           alt=""
         />
         <div className="absolute text-white left-0 top-0 h-[100vh] gap-4 w-full flex flex-col justify-center items-center">
-          <h3 className="text-3xl font-semibold">James Dylan White</h3>
-          <h4 className="text-2xl">Full Stack Software Engineer</h4>
+          <h3 className="text-4xl lg:text-6xl font-semibold">
+            James Dylan White
+          </h3>
+          <h4 className="text-2xl lg:text-4xl">Full Stack Software Engineer</h4>
         </div>
       </section>
+
+      <section id="about-me" className="container mx-auto my-16">
+        <div className="flex flex-col gap-16 p-4">
+          <h1 className="text-6xl font-normal">Welcome.</h1>
+
+          <p className="text-xl">
+            I am James White, a full stack software engineer based in Auckland,
+            New Zealand.
+          </p>
+        </div>
+      </section>
+
+      <footer>
+        <div className="container mx-auto">
+          <div className="flex justify-end items-center p-4">
+            <div className="flex gap-4">
+              <a href="https://www.linkedin.com/in/james-dylan-white-8a2b6a1b2/">
+                <FaLinkedin size={48} />
+              </a>
+              <a href="https://www.linkedin.com/in/james-dylan-white-8a2b6a1b2/">
+                <FaGithub size={48} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
       {/*
-      <AboutMe/>
       <Portfolio/>
       <Technologies/>
       <Footer /> */}
